9374a9ebba858bbd31dafca15cfdbd891aeadf9e
cbr400r <kazuya.3656@gmail.com>
2021-08-30 15:53:36 +0900

    コメント投稿機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :prototypes
   has_many :comments
+
   validates :name, presence: true
   validates :occupation, presence: true
+  validates :profile, presence: true
   validates :profile, presence: true
 end
 
--- db/migrate/20210825074602_create_comments.rb
+++ db/migrate/20210825074602_create_comments.rb
@@ -1,9 +1,11 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.text :text,             null: false
-      t.references :user,       null: false, foreign_key: true
-      t.references :prototype,  null: false, foreign_key: true
+      t.text :text,            null: false
+      t.integer :user_id
+      t.integer :prototype_id
+      t.references :user     , foreign_key: true
+      t.references :prototype, foreign_key: true
       t.timestamps
     end
   end
